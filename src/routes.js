fa8b8fe735c79f42b80cbae5335a058ed77a1aa2
Jose Rodolfo P. Pessanha da Silva <jrodolfo@auxiliadora.censanet.com.br>
2019-08-30 18:29:56 -0300

    Created routes to CRUD of categories

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,13 @@ const routes = express.Router();
 
 const CategoryController = require('./app/controllers/CategoryController');
 
+/**
+ * Categories
+ */
+routes.get('/categories', CategoryController.index);
+routes.get('/categories/:id', CategoryController.show);
 routes.post('/categories', CategoryController.store);
+routes.put('/categories/:id', CategoryController.update);
+routes.delete('/categories/:id', CategoryController.delete);
 
 module.exports = routes;
